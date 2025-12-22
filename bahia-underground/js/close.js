24a882a82beac80f9fa9d5b64c8e07a638daf1b5
Nêio Mustafa <neiolm@gmail.com>
2025-12-22 01:57:53 -0300

    javascript com senha para a webpage da recompnsa PLAYLIST
    Implement password protection for access control.

--- bahia-underground/js/close.js
+++ bahia-underground/js/close.js
@@ -1,1 +1,16 @@
+// JavaScript Document
+
+
+const senhaCorreta = "bahiaunderground2025";
+
+if (!sessionStorage.getItem("acessoLiberado")) {
+  const entrada = prompt("Digite a senha de acesso:");
+
+  if (entrada === senhaCorreta) {
+    sessionStorage.setItem("acessoLiberado", "true");
+  } else {
+    alert("Acesso restrito aos apoiadores.");
+    document.body.innerHTML = "";
+  }
+}
 
